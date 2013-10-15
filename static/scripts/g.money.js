8d2d6f4bd496ed8376b64bc9b77a83a47d014e6a
NetOperatorWibby <paul@dsgn.pw-software.com>
2013-10-15 13:12:47 -0400

    Added Gapelia menu to user profile

--- static/scripts/g.money.js
+++ static/scripts/g.money.js
@@ -13,6 +13,83 @@ $(document).ready(function() {
 		}
 	});
 
-	$("#user-book-list .book").css("height", $vH - 100 + "px");
+	// Set height of books in feed
+	$("#user-book-list .book").css("height", $vH - 97 + "px"); // 100
+	$("#user-collection-list .book").css("height", $vH - 97 + "px");
+	$("#user-library-list .book").css("height", $vH - 97 + "px");
+
+	$(".super-wrapper").css("height", $vH + "px");
+
+	$(".mp-pushed #g-menu-trigger").css("opacity", "1");
+
+	// Resize Image to Parent Container by Christian Varga
+	jQuery.fn.resizeToParent = function(options) {
+
+		var defaults = { parent: ".book" };
+		var options = jQuery.extend(defaults, options);
+
+		return this.each(function() {
+
+			var o = options;
+			var obj = jQuery(this);
+
+			// bind to load of image
+			obj.load(function() {
+
+				// dimensions of the parent
+				var parentWidth = obj.parents(o.parent).width();
+				var parentHeight = obj.parents(o.parent).height();
+
+				// dimensions of the image
+				var imageWidth = obj.width();
+				var imageHeight = obj.height();
+
+				// step 1 - calculate the percentage difference between image width and container width
+				var diff = imageWidth / parentWidth;
+
+				// step 2 - if height divided by difference is smaller than container height, resize by height. otherwise resize by width
+				if ((imageHeight / diff) < parentHeight) {
+					obj.css({"width": "auto", "height": parentHeight});
+
+					// set image variables to new dimensions
+					imageWidth = imageWidth / (imageHeight / parentHeight);
+					imageHeight = parentHeight;
+				} else {
+					obj.css({"height": "auto", "width": parentWidth});
+
+					// set image variables to new dimensions
+					imageWidth = parentWidth;
+					imageHeight = imageHeight / diff;
+				}
+
+				// step 3 - center image in container
+				var leftOffset = (imageWidth - parentWidth) / -2;
+				var topOffset = (imageHeight - parentHeight) / -2;
+
+				obj.css({ "left": leftOffset, "top": topOffset });
+
+			});
+
+		});
+
+	};
+
+	/*
+	$(function() {
+
+		$('.image').resizeToParent();
+
+		$('#showOverflow').click(function(e) {
+			e.preventDefault();
+			$('.imageContainer').css({'overflow': 'visible'});
+		});
+
+		$('#hideOverflow').click(function(e) {
+			e.preventDefault();
+			$('.imageContainer').css({'overflow': 'hidden'});
+		});
+
+	});
+	*/
 
 });
