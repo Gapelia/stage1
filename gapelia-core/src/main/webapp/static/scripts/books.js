a6c43ebac34dee0d0b059476903cb987c972b48c
NetOperatorWibby <paul@dsgn.pw-software.com>
2013-11-25 18:37:50 -0500

    Book creation work, trying to get geotags to work

--- gapelia-core/src/main/webapp/static/scripts/books.js
+++ gapelia-core/src/main/webapp/static/scripts/books.js
@@ -1,23 +1,60 @@
+
 	// Book Creation
 	// @Gapelia
 	// ====================================================================================
 	// Left Menus | Layout and page interaction | Right Menus | Content Creation
 	// Live Preview
+
 	// Globals
 	var $vW = $(window).width(), $vH = $(window).height();
-		var author;
-	var pageNumber;
-	var geotag;
-	var text;
-	var imageURl;
-	var videoURL;
-	var templateId;
-	var title;
-	var index;
-	var pages;
-	var book;
-	var pages;
-	var one;
+	var author, pageNumber, text, imageURL, videoURL, templateId, title, index, pages, book, pages, one;
+
+	/*
+	var
+	// geotag01 = document.getElementById("geotag-01"),
+	geotag01 = document.getElementsByClassName("page-geotag-elem"),
+	geotag02 = document.getElementById("geotag-02"),
+	geotag03 = document.getElementById("geotag-03"),
+	geotag04 = document.getElementById("geotag-04"),
+	geotag05 = document.getElementById("geotag-05"),
+	geotag06 = document.getElementById("geotag-06"),
+	geotag07 = document.getElementById("geotag-07"),
+	geotag08 = document.getElementById("geotag-08"),
+	geotag09 = document.getElementById("geotag-09"),
+	geotag10 = document.getElementById("geotag-10"),
+	geotag11 = document.getElementById("geotag-11"),
+	geotag12 = document.getElementById("geotag-12"),
+	geotag13 = document.getElementById("geotag-13"),
+	geotag14 = document.getElementById("geotag-14"),
+	geotag15 = document.getElementById("geotag-15"),
+	geotag16 = document.getElementById("geotag-16"),
+	geotag17 = document.getElementById("geotag-17"),
+	geotag18 = document.getElementById("geotag-18"),
+	geotag19 = document.getElementById("geotag-19"),
+	geotag20 = document.getElementById("geotag-20");
+
+	autocomplete01 = new google.maps.places.Autocomplete(geotag01);
+	autocomplete02 = new google.maps.places.Autocomplete(geotag02);
+	autocomplete03 = new google.maps.places.Autocomplete(geotag03);
+	autocomplete04 = new google.maps.places.Autocomplete(geotag04);
+	autocomplete05 = new google.maps.places.Autocomplete(geotag05);
+	autocomplete06 = new google.maps.places.Autocomplete(geotag06);
+	autocomplete07 = new google.maps.places.Autocomplete(geotag07);
+	autocomplete08 = new google.maps.places.Autocomplete(geotag08);
+	autocomplete09 = new google.maps.places.Autocomplete(geotag09);
+	autocomplete10 = new google.maps.places.Autocomplete(geotag10);
+	autocomplete11 = new google.maps.places.Autocomplete(geotag11);
+	autocomplete12 = new google.maps.places.Autocomplete(geotag12);
+	autocomplete13 = new google.maps.places.Autocomplete(geotag13);
+	autocomplete14 = new google.maps.places.Autocomplete(geotag14);
+	autocomplete15 = new google.maps.places.Autocomplete(geotag15);
+	autocomplete16 = new google.maps.places.Autocomplete(geotag16);
+	autocomplete17 = new google.maps.places.Autocomplete(geotag17);
+	autocomplete18 = new google.maps.places.Autocomplete(geotag18);
+	autocomplete19 = new google.maps.places.Autocomplete(geotag19);
+	autocomplete20 = new google.maps.places.Autocomplete(geotag20);
+	*/
+
 	// Set menu height, necessary for scrollbar plugin
 	$("#pages-scroller").css("height", $vH - 52 + "px");
 	$("#layout-scroller").css("height", $vH - 52 + "px");
@@ -28,22 +65,47 @@
 	$(".phototext-preview-wrapper .page-desc").css("height", $vH - 185 + "px");
 	$(".vertical-preview-wrapper .page-desc").css("height", $vH - 185 + "px");
 	$(".video-preview-wrapper .page-desc").css("height", $vH - 185 + "px");
+
 	$(document).ready(function () {
-		geotag = document.getElementById("geotag");
-		autocomplete = new google.maps.places.Autocomplete(geotag);
- 		book = {"author":"AUTHOR","title":null,"library":"NULL","dimension":"NULL"};
- 		pages = {"page":[{"pageNumber":0,"geotag":geotag,"templateId":"0","title":null,"text":null,"image":"/static/images/blank-bg.jpg","video":"NULL"}]};
-		index=0;
-		author=book.author;
-		pageNumber=pages.page[index].pageNumber;
-		text=pages.page[index].text;
-		imageURl=pages.page[index].image;
-		videoURL=pages.page[index].video;
-		templateId=pages.page[index].templateId;
-		title=pages.page[index].title;
-		pages=1;
-		one =1;
+
+		// geotag = document.getElementById("geotag");
+		// geotag = document.getElementsByClassName("geotag");
+		// autocomplete = new google.maps.places.Autocomplete(geotag);
+
+ 		book = {
+			"author" : "AUTHOR",
+			"title" : null,
+			"library" : "NULL",
+			"dimension" : "NULL"
+		};
+
+ 		pages = {
+			"page" : [{
+				"pageNumber" : 0,
+				// "geotag" : geotag,
+				"geotag" : null,
+				"templateId" : "0",
+				"title" : null,
+				"text" : null,
+				"image" : "/static/images/blank-bg.jpg",
+				// "video" : "NULL"
+				"video" : ""
+			}]
+		};
+
+		index = 0;
+		author = book.author;
+		pageNumber = pages.page[index].pageNumber;
+		text = pages.page[index].text;
+		imageURL = pages.page[index].image;
+		videoURL = pages.page[index].video;
+		templateId = pages.page[index].templateId;
+		title = pages.page[index].title;
+		pages = 1;
+		one = 1;
+
 	});
+
 	// Left Menus
 	// @Gapelia
 	// ------------------------------------------------------------------------------------
@@ -104,39 +166,50 @@
 
 	// Toggle layout switcher
 	$("#select-frontcover-layout").click(function (e) {
-		var insert="";
-		var imageURl=$('#theImage').attr('src');
-		var text=$('#theText').text();
-		var title=$('#theTitle').text();
-		insert+="<section class=\"frontcover-preview-wrapper\">";
-		if(imageURl==null)
-		{
-			insert+="<img id=\"theImage\" class=\"page-bg\" src=\"static/images/blank-bg.jpg\"/>";
-		}
-		else{
-			insert+="<img id=\"theImage\" class=\"page-bg\" src=\""+imageURl+"\"/>";
-		}
-		insert+="<div class=\"button-wrapper\"><input id=\"me\" type=\"filepicker\" data-fp-apikey=\"ABFuSiQFbQRylrWy9nCs7z\" data-fp-mimetypes=\"image/*\" data-fp-container=\"window\" data-fp-multiple=\"true\"onchange=\"url=event.fpfile.url; console.log(url); $('#theImage').attr('src', url);\"></div>";
-		if(title==null){
-			insert+="<div class=\"frontcover-preview\"><article><h1 id=\"theTitle\" class=\"page-title-elem\"  placeholder=\"Write your title here\" contenteditable=\"true\"></h1>";
-		}
-		else{
-			insert+="<div class=\"frontcover-preview\"><article><h1 id=\"theTitle\" class=\"page-title-elem\" contenteditable=\"true\">"+title+"</h1>";
-		}
-		if(text==null){
-			insert+="<h5 contenteditable=\"false\"><span>* "+author+" *</span></h5><div id=\"theText\" class=\"page-desc\" placeholder=\"Start writing your story here.\" contenteditable=\"true\"></div></article></div></section>";
-		}
-		else{
-			insert+="<h5 contenteditable=\"false\"><span>* "+author+" *</span></h5><div id=\"theText\" class=\"page-desc\" contenteditable=\"true\">"+text+"</div></article></div></section>";
-		}
-		$("#create-content").html(insert);
-		templateId=0;
-		var element=$('#me');
-		element=element[0];
-		element.type="filepicker";
-		filepicker.constructWidget(element); 
-		/*
+
 		$(this).addClass("selected-layout");
+		// $(selector).not(".white")
+		// $(selector).is(":not(.white)")
+
+		var
+		insert = "",
+		imageURL = $(".page-bg").attr("src"),
+		title = $(".page-title-elem").text(),
+		text = $(".page-desc").text();
+
+		insert+="<section class=\"frontcover-preview-wrapper\">";
+
+		if(imageURL == null) {
+			insert += "<img class=\"page-bg\" src=\"static/images/blank-bg.jpg\"/>";
+		} else {
+			insert += "<img class=\"page-bg\" src=\"" +imageURL+ "\"/>";
+		}
+
+		insert += "<div class=\"button-wrapper\"><input class=\"photo-picker\" type=\"filepicker\" data-fp-apikey=\"ABFuSiQFbQRylrWy9nCs7z\" data-fp-mimetypes=\"image/*\" data-fp-container=\"modal\" data-fp-services=\"COMPUTER,BOX,DROPBOX,FACEBOOK,FLICKR,GOOGLE_DRIVE\" onchange=\"url=event.fpfile.url; console.log(url); $('.page-bg').attr('src', url);\"></div>";
+
+		if(title == null) {
+			insert += '<div class=\"frontcover-preview\"><article><h1 class=\"page-title-elem\" placeholder=\"Write your title here\" contenteditable=\"true\"></h1>';
+		} else {
+			insert += '<div class=\"frontcover-preview\"><article><h1 id=\"theTitle\" class=\"page-title-elem\" contenteditable=\"true\">' +title+ "</h1>";
+		}
+
+		if(text == null) {
+			insert += '<h5 contenteditable=\"false\"><span>*' +author+ '*</span></h5><div class=\"page-desc\" placeholder=\"Start writing your story here.\" contenteditable=\"true\"></div></article></div></section>';
+		} else {
+			insert += '<h5 contenteditable=\"false\"><span>*' +author+ '*</span></h5><div class=\"page-desc\" contenteditable=\"true\">' +text+ '</div></article></div></section>';
+		}
+
+		$("#create-content").html(insert);
+		templateId = 0;
+
+		var element = $(".photo-picker");
+		element = element[0];
+		element.type = "filepicker";
+		filepicker.constructWidget(element);
+
+		e.preventDefault();
+
+		/*
 		$("#select-photo-layout").removeClass("selected-layout");
 		$("#select-text-layout").removeClass("selected-layout");
 		$("#select-horizontal-layout").removeClass("selected-layout");
@@ -153,45 +226,83 @@
 		$("#test-phototext").hide();
 		$("#test-vertical").hide();
 		$("#test-video").hide();
-		e.preventDefault();
 		*/
 
 	});
 
 	$("#select-photo-layout").click(function (e) {
-		var insert="";
-		var imageURl=$('#theImage').attr('src');
-		var text=$('#theText').text();
-		var title=$('#theTitle').text();
-		insert+="<section class=\"photo-preview-wrapper\">";
-		if(imageURl==null)
-		{
-			insert+="<img id=\"theImage\" class=\"page-bg\" src=\"static/images/blank-bg.jpg\"/>";
+
+		$(this).addClass("selected-layout");
+
+		var
+		insert = "",
+		imageURL = $(".page-bg").attr("src"),
+		title = $(".page-title-elem").text(),
+		text = $(".page-desc").text();
+		// geotag = $("page-geotag-elem");
+
+		insert += "<section class=\"photo-preview-wrapper\">";
+
+		if(imageURL == null) {
+			insert += "<img class=\"page-bg\" src=\"static/images/blank-bg.jpg\"/>";
+		} else {
+			insert += "<img class=\"page-bg\" src=\"" +imageURL+ "\"/>";
 		}
-		else{
-			insert+="<img id=\"theImage\" class=\"page-bg\" src=\""+imageURl+"\"/>";
+
+		insert += "<div class=\"button-wrapper\"><input class=\"photo-picker\" type=\"filepicker\" data-fp-apikey=\"ABFuSiQFbQRylrWy9nCs7z\" data-fp-mimetypes=\"image/*\" data-fp-container=\"modal\" data-fp-services=\"COMPUTER,BOX,DROPBOX,FACEBOOK,FLICKR,GOOGLE_DRIVE\" onchange=\"url=event.fpfile.url; console.log(url); $('.page-bg').attr('src', url);\"></div>";
+
+		if(title == null) {
+			insert += '<div class=\"photo-preview\"><article><h1 class=\"page-title-elem\" placeholder=\"Write your title here\" contenteditable=\"true\"></h1>';
+		} else {
+			insert += '<div class=\"photo-preview\"><article><h1 class=\"page-title-elem\" contenteditable=\"true\">' +title+ "</h1>";
 		}
-		insert+="<div class=\"button-wrapper\"><input id=\"me\" type=\"filepicker\" data-fp-apikey=\"ABFuSiQFbQRylrWy9nCs7z\" data-fp-mimetypes=\"image/*\" data-fp-container=\"window\" data-fp-multiple=\"true\"onchange=\"url=event.fpfile.url; console.log(url); $('#theImage').attr('src', url);\"></div>";
-		if(title==null){
-			insert+="<div class=\"photo-preview\"><article><h1 class=\"page-title-elem\"  id=\"theTitle\" placeholder=\"Write your title here\" contenteditable=\"true\"></h1>";
+
+		insert += '<input class=\"page-geotag-elem\" placeholder=\"Select your location\"/>';
+
+		/*
+		var incrementVar = 0;
+
+		// var value = parseInt($(":text[name='qty']").val()) + 1;
+		var value = 0 + 1;
+		$(".page-geotag-elem").attr("id", "geotag" + value);
+		incrementVar = incrementVar + value;
+		*/
+
+		if(text == null) {
+			insert += '<div class=\"page-desc\" contenteditable=\"true\" placeholder=\"Start writing your story here.\"></div></article></div></section>';
+		} else {
+			insert += '<div class=\"page-desc\" contenteditable=\"true\">' +text+ '</div></article></div></section>';
 		}
-		else{
-			insert+="<div class=\"photo-preview\"><article><h1 class=\"page-title-elem\" id=\"theTitle\" contenteditable=\"true\">"+title+"</h1>";
-		}
-		insert+="<input id=\"geotag\" class=\"page-geotag-elem\" placeholder=\"Select your location\"/>";
-		if(text==null){
-			insert+="<div class=\"page-desc\" contenteditable=\"true\" id=\"theText\" placeholder=\"Start writing your story here.\"></div></article></div></section>";
-		}
-		else{
-			insert+="<div class=\"page-desc\" id=\"theText\"  contenteditable=\"true\">"+text+"</div></article></div></section>";
-		}
-		
+
 		$("#create-content").html(insert);
-		templateId=1;
-		var element=$('#me');
-		element=element[0];
-		element.type="filepicker";
-		filepicker.constructWidget(element); 
+		templateId = 1;
+
+		geotag = document.getElementsByClassName("page-geotag-elem");
+		autocomplete = new google.maps.places.Autocomplete(geotag);
+
+		var element = $(".photo-picker");
+		element = element[0];
+		element.type = "filepicker";
+		filepicker.constructWidget(element);
+
+		/*
+		$("#select-frontcover-layout").removeClass("selected-layout");
+		$("#select-text-layout").removeClass("selected-layout");
+		$("#select-horizontal-layout").removeClass("selected-layout");
+		$("#select-overlay-layout").removeClass("selected-layout");
+		$("#select-phototext-layout").removeClass("selected-layout");
+		$("#select-vertical-layout").removeClass("selected-layout");
+		$("#select-video-layout").removeClass("selected-layout");
+
+		$("#test-frontcover").hide();
+		$("#test-photo").toggle();
+		$("#test-text").hide();
+		$("#test-horizontal").hide();
+		$("#test-overlay").hide();
+		$("#test-phototext").hide();
+		$("#test-vertical").hide();
+		$("#test-video").hide();
+		*/
 
 		// Google Maps Autocomplete list positioning
 		$(".pac-container").css("margin-top", "-210px").css("position", "absolute");
@@ -576,6 +687,7 @@
 	});
 
 	// MUST FIX!!!
+	/*
 	$(".back-cover").mousemove(function() {
 	// $(".back-cover").ready(function() {
 
@@ -599,6 +711,82 @@
 		// if $('.back-cover').css('display');
 
 	});
+	*/
+
+	// MUST FIX!!!
+	// $(".back-cover").mousemove(function() {
+
+	$(function() {
+		setTimeout(function() {
+
+			if ($("#page1[style*='display:block']")) {
+				// if ($("#page1").attr("style") == "block") {
+				$("#bb-nav-prev").hide();
+			} else {
+				$("#bb-nav-prev").show();
+			}
+
+			/*
+			$(".front-cover").ready(function() {
+
+				if ($(".back-cover").css("display")) {
+					$("#bb-nav-prev").css({
+						"opacity": "0",
+						"right": "-50px"
+					});
+				}
+
+			});
+
+			$(".back-cover").ready(function() {
+
+				if ($(".back-cover").css("display")) {
+					$("#header-toggle").css({
+						"opacity": "0",
+						"top": "-50px"
+					});
+
+					$("#next-book-toggle").css({
+						"opacity": "0",
+						"top": "-50px"
+					});
+
+					$("#bb-nav-next").css({
+						"opacity": "0",
+						"right": "-50px"
+					});
+				}
+
+			});
+			*/
+
+		}, 30);
+	});
+
+	/*
+	$(".back-cover").ready(function() {
+
+		if ($(".back-cover").css("display")) {
+			$("#header-toggle").css({
+				"opacity": "0",
+				"top": "-50px"
+			});
+
+			$("#next-book-toggle").css({
+				"opacity": "0",
+				"top": "-50px"
+			});
+
+			$("#bb-nav-next").css({
+				"opacity": "0",
+				"right": "-50px"
+			});
+		}
+
+		// if $('.back-cover').css('display');
+
+	});
+	*/
 
 	$(".video-preview .play-video").click(function () {
 
--- gapelia-core/src/main/webapp/templates/create/index.jsp
+++ gapelia-core/src/main/webapp/templates/create/index.jsp
@@ -1,9 +1,12 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 	<head>
 
 		<meta charset="utf-8"/>
 		<title>Create a book | Gapelia</title>
+
 		<!--/
 			 ______   ______   ______  ______   __       __   ______    
 			/\  ___\ /\  __ \ /\  == \/\  ___\ /\ \     /\ \ /\  __ \   
@@ -23,18 +26,18 @@
 		<meta name="description" content="Better stories, together."/>
 		<meta name="keywords" content="Gapelia, storytelling, lifestyle, story, creator, travel, pulse, art, wow, life, flow, wonder, dimension"/>
 
-		<link href="static/css/style.css" rel="stylesheet"/>
-		<link href="static/css/selectize.css" rel="stylesheet"/>
-		<link href="static/images/favicon.png" rel="shortcut icon"/>
+		<link href="/static/css/style.css" rel="stylesheet"/>
+		<link href="/static/css/selectize.css" rel="stylesheet"/>
+		<link href="/static/images/favicon.png" rel="shortcut icon"/>
 
 		<!--* if lt IE 9 *>
 			<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<!* endif *-->
 
-		<script src="static/scripts/jquery-2.0.3.min.js"></script>
-		<script src="static/scripts/selectize.js"></script>
+		<script src="/static/scripts/jquery-2.0.3.min.js"></script>
+		<script src="/static/scripts/selectize.js"></script>
 
-		<script src="http://use.typekit.net/web3vzl.js"></script>
+		<script src="//use.typekit.net/web3vzl.js"></script>
 		<script>try{Typekit.load();}catch(e){}</script>
 
 		<script>
@@ -66,6 +69,7 @@
 	</head>
 
 	<body class="book-creation g-body">
+
 		<header>
 			<div id="back">
         <div id="pages-toggle"><a href="#" title="Add pages">&oplus;</a></div>
@@ -90,10 +94,39 @@
 							<a class="delete-page entypo">&#9749;</a>
 
 							<section>
-								<img src="static/images/space-bb-small.JPG" id="page-01-thumb" alt=""/>
+								<img src="/static/images/space-bb-small.JPG" id="page-01-thumb" alt=""/>
 								<span class="livepreview-thing">01 &middot; Front Cover</span>
 							</section>
 						</li>
+
+						<!--/
+						<li class="page-delete-demo">
+							<a class="delete-page entypo">&#9749;</a>
+
+							<a href="">
+								<img src="/static/images/new-page-thumb.png" id="page-02-thumb" alt=""/>
+								<span>02 &middot; Beautiful Skyline in Tokyo Bay</span>
+							</a>
+						</li>
+
+						<li>
+							<a href="#" class="delete-page entypo">&#9749;</a>
+
+							<a href="">
+								<img src="/static/images/new-page-thumb.png" id="page-03-thumb" alt=""/>
+								<span>03 &middot; Underground Passage</span>
+							</a>
+						</li>
+
+						<li>
+							<a href="#" class="delete-page entypo">&#9749;</a>
+
+							<a href="">
+								<img src="/static/images/new-page-thumb.png" id="page-04-thumb" alt=""/>
+								<span>04 &middot; New Page</span>
+							</a>
+						</li>
+						/-->
 
 						<li id="add-page" class="new-thumb disable-sort">
 							<a href="#">+</a>
@@ -108,42 +141,42 @@
 				<div id="layout-scroller">
 					<ul>
 						<li id="select-frontcover-layout">
-							<img src="static/images/view-modes/front.png" alt=""/>
+							<img src="/static/images/view-modes/front.png" alt=""/>
 							<span>Front Cover</span>
 						</li>
 
 						<li id="select-photo-layout">
-							<img src="static/images/view-modes/photo.png" alt=""/>
+							<img src="/static/images/view-modes/photo.png" alt=""/>
 							<span>Photo</span>
 						</li>
 
 						<li id="select-text-layout">
-							<img src="static/images/view-modes/text.png" alt=""/>
+							<img src="/static/images/view-modes/text.png" alt=""/>
 							<span>Text</span>
 						</li>
 
 						<li id="select-horizontal-layout">
-							<img src="static/images/view-modes/horizontal.png" alt=""/>
+							<img src="/static/images/view-modes/horizontal.png" alt=""/>
 							<span>Horizontal</span>
 						</li>
 
 						<li id="select-overlay-layout">
-							<img src="static/images/view-modes/overlay.png" alt=""/>
+							<img src="/static/images/view-modes/overlay.png" alt=""/>
 							<span>Overlay</span>
 						</li>
 
 						<li id="select-phototext-layout">
-							<img src="static/images/view-modes/phototext.png" alt=""/>
+							<img src="/static/images/view-modes/phototext.png" alt=""/>
 							<span>Photo/Text</span>
 						</li>
 
 						<li id="select-vertical-layout">
-							<img src="static/images/view-modes/vertical.png" alt=""/>
+							<img src="/static/images/view-modes/vertical.png" alt=""/>
 							<span>Vertical</span>
 						</li>
 
 						<li id="select-video-layout">
-							<img src="static/images/view-modes/video.png" alt=""/>
+							<img src="/static/images/view-modes/video.png" alt=""/>
 							<span>Video</span>
 						</li>
 					</ul>
@@ -190,6 +223,7 @@
 						</select>
 					</div>
 				</div>
+
 				<div id="create-book">
 					<!--/ <div id="create-content" data-role="flip" data-flip-show-pager="false" data-flip-forward-dir="rtol"/> /-->
 					<div id="create-content">
@@ -210,9 +244,11 @@
 
 						<!--/ Front Cover /-->
 						<section id="test-frontcover" class="frontcover-preview-wrapper" style="display: none;">
-							<img id="page-01-placeholder" class="page-bg" src="static/images/blank-bg.jpg"/>
+							<img id="page-01-placeholder" class="page-bg" src="/static/images/blank-bg.jpg" alt=""/>
 
-							<div class=\"button-wrapper\"><button id=\"pickButton\">Pick File</button>
+							<div class="button-wrapper">
+								<input class="photo-picker" type="filepicker" data-fp-apikey="ABFuSiQFbQRylrWy9nCs7z" data-fp-mimetypes="image/*" data-fp-container="modal" data-fp-services="COMPUTER,BOX,DROPBOX,FACEBOOK,FLICKR,GOOGLE_DRIVE" onchange="url=event.fpfile.url; console.log(url); $('#page-01-placeholder').attr('src', url);">
+							</div>
 
 							<div class="frontcover-preview">
 								<article class="cover-info">
@@ -228,7 +264,7 @@
 
 						<!--/ Photo /-->
 						<section id="test-photo" class="photo-preview-wrapper" style="display: none;">
-							<img id="page-02-placeholder" class="page-bg" src="static/images/blank-bg.jpg"/>
+							<img id="page-02-placeholder" class="page-bg" src="/static/images/blank-bg.jpg" alt=""/>
 
 							<div class="button-wrapper">
 								<input class="photo-picker" type="filepicker" data-fp-apikey="ABFuSiQFbQRylrWy9nCs7z" data-fp-mimetypes="image/*" data-fp-container="modal" data-fp-services="COMPUTER,BOX,DROPBOX,FACEBOOK,FLICKR,GOOGLE_DRIVE" onchange="url=event.fpfile.url; console.log(url); $('#page-02-placeholder').attr('src', url);">
@@ -263,7 +299,7 @@
 						<!--/ Horizontal /-->
 						<section id="test-horizontal" class="horizontal-preview-wrapper" style="display: none;">
 							<section class="draggable-placeholder"><!--/ style="background: url('/static/images/space-bb.jpg');" /-->
-								<img id="page-03-placeholder" class="page-bg" src="static/images/blank-bg.jpg"/>
+								<img id="page-03-placeholder" class="page-bg" src="/static/images/blank-bg.jpg" alt=""/>
 
 								<div class="button-wrapper">
 									<input class="photo-picker" type="filepicker" data-fp-apikey="ABFuSiQFbQRylrWy9nCs7z" data-fp-mimetypes="image/*" data-fp-container="modal" data-fp-services="COMPUTER,BOX,DROPBOX,FACEBOOK,FLICKR,GOOGLE_DRIVE" onchange="url=event.fpfile.url; console.log(url); $('#page-03-placeholder').attr('src', url);">
@@ -284,7 +320,7 @@
 
 						<!--/ Overlay /-->
 						<section id="test-overlay" class="overlay-preview-wrapper" style="display: none;">
-							<img id="page-04-placeholder" class="page-bg" src="static/images/blank-bg.jpg"/>
+							<img id="page-04-placeholder" class="page-bg" src="/static/images/blank-bg.jpg" alt=""/>
 
 							<div class="button-wrapper">
 								<input class="photo-picker" type="filepicker" data-fp-apikey="ABFuSiQFbQRylrWy9nCs7z" data-fp-mimetypes="image/*" data-fp-container="modal" data-fp-services="COMPUTER,BOX,DROPBOX,FACEBOOK,FLICKR,GOOGLE_DRIVE" onchange="url=event.fpfile.url; console.log(url); $('#page-04-placeholder').attr('src', url);">
@@ -300,17 +336,19 @@
 							</div>
 						</section>
 						<!--//Overlay /-->
-
+ 
 						<!--/ Photo/Text /-->
 						<section id="test-phototext" class="phototext-preview-wrapper" style="display: none;">
-							<img id="page-05-placeholder" class="page-bg" src="static/images/blank-bg.jpg"/>
+							<img id="page-05-placeholder" class="page-bg" src="/static/images/blank-bg.jpg" alt=""/>
 
-							<div class=\"button-wrapper\"><button onclick=\"javascript:openFilePicker()\">Choose File</button></div>
+							<div class="button-wrapper">
+								<input class="photo-picker" type="filepicker" data-fp-apikey="ABFuSiQFbQRylrWy9nCs7z" data-fp-mimetypes="image/*" data-fp-container="modal" data-fp-services="COMPUTER,BOX,DROPBOX,FACEBOOK,FLICKR,GOOGLE_DRIVE" onchange="url=event.fpfile.url; console.log(url); $('#page-05-placeholder').attr('src', url);">
+							</div>
 
 							<div class="phototext-preview">
 								<article>
 
-									<h1 class="page-title-elem"  data-placeholder="Write your title here"></h1>
+									<h1 class="page-title-elem" data-placeholder="Write your title here"></h1>
 									<input id="geotag-05" class="page-geotag-elem" placeholder="Select your location"/>
 									<div class="page-desc" data-placeholder="Start writing your story here."></div>
 
@@ -322,7 +360,7 @@
 						<!--/ Vertical /-->
 						<section id="test-vertical" class="vertical-preview-wrapper" style="display: none;">
 							<section class="draggable-placeholder"><!--/ style="background: url('/static/images/space-bb.jpg');" /-->
-								<img id="page-06-placeholder" class="page-bg" src="static/images/blank-bg.jpg"/>
+								<img id="page-06-placeholder" class="page-bg" src="/static/images/blank-bg.jpg" alt=""/>
 
 								<div class="button-wrapper">
 									<input class="photo-picker" type="filepicker" data-fp-apikey="ABFuSiQFbQRylrWy9nCs7z" data-fp-mimetypes="image/*" data-fp-container="modal" data-fp-services="COMPUTER,BOX,DROPBOX,FACEBOOK,FLICKR,GOOGLE_DRIVE" onchange="url=event.fpfile.url; console.log(url); $('#page-06-placeholder').attr('src', url);">
@@ -451,15 +489,18 @@
 		<!--//dialog-windows /-->
 
 		<!--/ scripts /-->
-		<script src="static/scripts/grande.js"></script>
+		<script src="/static/scripts/grande.js"></script>
+
 		<script>
 			grande.bind(document.querySelectorAll(".page-title-elem"));
 			grande.bind(document.querySelectorAll(".page-desc"));
 		</script>
 
-		<script src="static/scripts/filepicker2.js"></script>
-		<script src="static/scripts/books.js"></script>
-		<script src="static/scripts/draggable_background.js"></script>
+		<script src="/static/scripts/filepicker2.js"></script>
+		<script src="http://maps.google.com/maps/api/js?key=AIzaSyDTyK4a-ZbTbi1LWWOBOowJfL7k4J6OX8Y&amp;libraries=places&amp;sensor=false"></script>
+		<script src="/static/scripts/books.js"></script>
+
+		<script src="/static/scripts/draggable_background.js"></script>
 
 		<script>
 			// Horizontal and Vertical layouts
@@ -467,8 +508,8 @@
 		</script>
 
 		<!--/ scripts/layout-scroller /-->
-		<script src="static/scripts/jquery.mousewheel.js"></script>
-		<script src="static/scripts/scrollpanel.js"></script>
+		<script src="/static/scripts/jquery.mousewheel.js"></script>
+		<script src="/static/scripts/scrollpanel.js"></script>
 
 		<script>
 			$("#pages-scroller").scrollpanel();
@@ -483,7 +524,7 @@
 		</script>
 
 		<!--/ scripts/page-sorter /-->
-		<script src="static/scripts/sortable.js"></script>
+		<script src="/static/scripts/sortable.js"></script>
 
 		<script>
 			$(function() {
@@ -492,8 +533,9 @@
 			});
 		</script>
 
-		<script src="http://maps.google.com/maps/api/js?key=AIzaSyDTyK4a-ZbTbi1LWWOBOowJfL7k4J6OX8Y&amp;libraries=places&amp;sensor=false"></script>
+		<!--/ <script src="http://maps.google.com/maps/api/js?key=AIzaSyDTyK4a-ZbTbi1LWWOBOowJfL7k4J6OX8Y&amp;libraries=places&amp;sensor=false"></script> /-->
 
+		<!--/
 		<script>
 			var geotag01 = document.getElementById("geotag-01");
 			autocomplete = new google.maps.places.Autocomplete(geotag01);
@@ -555,10 +597,11 @@
 			var geotag20 = document.getElementById("geotag-20");
 			autocomplete = new google.maps.places.Autocomplete(geotag20);
 		</script>
+		/-->
 		
 		<!--/ scripts/dialog /-->
-		<script src="static/scripts/vex.js"></script>
-		<script src="static/scripts/vex.dialog.js"></script>
+		<script src="/static/scripts/vex.js"></script>
+		<script src="/static/scripts/vex.dialog.js"></script>
 
 		<script>
 			var modal = {};
