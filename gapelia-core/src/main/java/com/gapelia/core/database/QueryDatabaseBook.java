80d294a67a46f5f70406bba6be9048b6475c3369
Daniel Campos <dcampos@basistech.com>
2014-02-24 14:36:26 -0500

    changes sql file

--- gapelia-core/src/main/java/com/gapelia/core/database/QueryDatabaseBook.java
+++ gapelia-core/src/main/java/com/gapelia/core/database/QueryDatabaseBook.java
@@ -14,4 +14,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class QueryDatabaseBook {
+    private static Logger LOG = Logger.getLogger(Book.class);
+    private static Connection connection = DatabaseManager.getInstance().getConnection();
+    //Page Relate Queries
+    private static final String 
+    private static final String UPDATE_PAGE = "UPDATE pages set title = ?, description = ?, templateId = ?, bookId = ?, marginX = ?, marginY = ?, videoUrl = ?, pageNumber = ?, userId = ?, photoUrl = ?, photoId = ?, creativeCommons = ?, lastUpdated = ? WHERE pageId = ?";
+    private static final String INSERT_PAGE = "INSERT INTO pages (title, description,templateId,bookId,marginX,marginY,videoUrl,pageNumber,userId,photoUrl,photoId,pageId) "+ "VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+    // Book Related Queries
+    private static final String INSERT_BOOK = "INSERT INTO books (bookiD,title, language,library,tags,userId,isPublished,coverPhoto) " + "VALUES(?,?,?,?,?,?,?,?)";
+    private static final String UPDATE_BOOK = "UPDATE books set coverPhoto = ?, title = ?, language = ?, library = ?, tags = ?, lastUpdated = ?, isPublished = ? WHERE bookId= ?";
+
 }
