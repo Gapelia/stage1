7607f94a9e655d817c2bcae02a592e11679efbd4
NetOperatorWibby <paul@dsgn.io>
2014-02-25 16:52:46 -0500

    Optimizing

--- gapelia-core/src/main/webapp/static/scripts/g.money.js
+++ gapelia-core/src/main/webapp/static/scripts/g.money.js
@@ -207,6 +207,15 @@
 		$("#create-library").click(function (e) {
 
 			$("#create-library-overlay").addClass("open");
+
+			$("#mp-pusher").css({
+				"transform": "translate3d(0, 0, 0)",
+				"-o-transform": "translate3d(0, 0, 0)",
+				"-ms-transform": "translate3d(0, 0, 0)",
+				"-moz-transform": "translate3d(0, 0, 0)",
+				"-webkit-transform": "translate3d(0, 0, 0)"
+			});
+
 			e.preventDefault();
 
 		});
