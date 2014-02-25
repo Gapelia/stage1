ca790f52f0475ad2eda05952a01029088cae1072
Daniel Campos <dfcspoor@hotmail.com>
2014-02-25 17:05:55 -0500

    loging

--- gapelia-core/src/main/java/com/gapelia/core/database/QueryDatabaseUser.java
+++ gapelia-core/src/main/java/com/gapelia/core/database/QueryDatabaseUser.java
@@ -58,7 +58,7 @@ public class QueryDatabaseUser {
             insert.setString(7, profile.getProfileImageURL());
             insert.setString(8, profile.getFirstName());
             insert.setInt(9, Integer.parseInt(profile.getValidatedId()));
-            insert.setString(10, profile.getProviderId());
+            insert.setInt(10, Integer.parseInt(profile.getProviderId()));
             insert.setDate(11, new Date(System.currentTimeMillis()));
             insert.setDate(12, new Date(System.currentTimeMillis()));
             insert.setDate(13, new Date(System.currentTimeMillis()));
