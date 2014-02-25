c798b2c37f36d3df18af01e7e0b923bc0b504ec0
Daniel Campos <dfcspoor@hotmail.com>
2014-02-24 19:51:07 -0500

    commented code out to work with maven

--- gapelia-core/src/main/java/com/gapelia/core/api/Libraries.java
+++ gapelia-core/src/main/java/com/gapelia/core/api/Libraries.java
@@ -16,160 +16,160 @@ public class Libraries {
 
 	public static Logger LOG = Logger.getLogger(Libraries.class);
 
-	@Path("getFeaturedBooks")
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public String getFeaturedBooks(@FormParam("sessionId") String sessionId,
-								   @FormParam("dimension") String dimension) {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		try {
-			// Get UserId from SessionId
-			LOG.info("Trying to retrieve user from session id");
-			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
-			// Retrieving user details
-			LOG.info("Trying to retrieve featured top books for user");
-			Book[] books = QueryDatabase.getFeaturedBooks(profile);
-			String json = gson.toJson(books);
-			LOG.info("Response json: " + json);
-			return json;
-		} catch (Exception ex) {
-			LOG.error("Failed to retrieve top featured books", ex);
-			return gson.toJson("Failed");
-		}
-	}
-
-	@Path("getAllBooks")
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public String getAllBooks(@FormParam("sessionId") String sessionId,
-							  @FormParam("dimension") String dimension,
-							  @FormParam("pageNo") String page) {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		try {
-			// Get UserId from SessionId
-			LOG.info("Trying to retrieve user from session id");
-			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
-			// Retrieving user details
-			LOG.info("Trying to retrieve all books for user");
-			Book[] books = QueryDatabase.getAllBooks();
-			String json = gson.toJson(books);
-			//String json = gson.toJson("NOT HERE");
-			LOG.info("Response json: " + json);
-			return json;
-		} catch (Exception ex) {
-			LOG.error("Failed to retrieve all books", ex);
-			return gson.toJson("Failed"+ex.toString());
-		}
-	}
-
-	@Path("getLibrary")
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public String getLibrary(@FormParam("sessionId") String sessionId,
-							  @FormParam("libraryId") String libraryId) {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		try {
-			// Get UserId from SessionId
-			LOG.info("Trying to retrieve user from session id");
-			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
-			// Retrieving user details
-			LOG.info("Trying to retrieve library");
-			Library library = QueryDatabase.getLibraryById(profile, libraryId);
-			String json = gson.toJson(library);
-			LOG.info("Response json: " + json);
-			return json;
-		} catch (Exception ex) {
-			LOG.error("Failed to retrieve library", ex);
-			return gson.toJson("Failed");
-		}
-	}
-
-	@Path("getAllLibraries")
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public String getAllLibraries(@FormParam("sessionId") String sessionId,
-								  @FormParam("pageNo") String page) {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		try {
-			// Get UserId from SessionId
-			LOG.info("Trying to retrieve user from session id");
-			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
-			// Retrieving user details
-			Library [] libraries = QueryDatabase.getAllLibraries(profile, page);
-			String json = gson.toJson(libraries);
-			LOG.info("Response json: " + json);
-			return json;
-		} catch (Exception ex) {
-			LOG.error("Failed to retrieve library", ex);
-			return gson.toJson("Failed");
-		}
-	}
-
-	@Path("getLibrariesByPrefix")
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public String getLibrariesByPrefix(@FormParam("sessionId") String sessionId,
-									   @FormParam("prefix") String prefix) {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		try {
-			// Get UserId from SessionId
-			LOG.info("Trying to retrieve user from session id");
-			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
-			// Retrieving user details
-			Library [] libraries = QueryDatabase.getLibraryByPrefix(profile, prefix);
-			String json = gson.toJson(libraries);
-			LOG.info("Response json: " + json);
-			return json;
-		} catch (Exception ex) {
-			LOG.error("Failed to retrieve library", ex);
-			return gson.toJson("Failed");
-		}
-	}
-
-	@Path("subscribe")
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public String subscribe(@FormParam("sessionId") String sessionId,
-							@FormParam("libraryId") String libraryId
-	) {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		try {
-			// Get UserId from SessionId
-			LOG.info("Trying to retrieve user id from session id");
-			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
-			LOG.info("Trying to subscribe");
-			boolean status = QueryDatabase.subscribeLibrary(profile, libraryId);
-			return gson.toJson(status ? "Success" : "Failure");
-		} catch (Exception ex) {
-			LOG.error("Failed to subscribe", ex);
-			return gson.toJson("Failed");
-		}
-	}
-
-	@Path("unsubscribe")
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public String unsubscribe(@FormParam("sessionId") String sessionId,
-							@FormParam("libraryId") String libraryId
-	) {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		try {
-			// Get UserId from SessionId
-			LOG.info("Trying to retrieve user id from session id");
-			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
-			LOG.info("Trying to un-subscribe");
-			boolean status = QueryDatabase.unSubscribeLibrary(profile, libraryId);
-			return gson.toJson(status ? "Success" : "Failure");
-		} catch (Exception ex) {
-			LOG.error("Failed to unsubscribe", ex);
-			return gson.toJson("Failed");
-		}
-	}
+//	@Path("getFeaturedBooks")
+//	@POST
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//	public String getFeaturedBooks(@FormParam("sessionId") String sessionId,
+//								   @FormParam("dimension") String dimension) {
+//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//		try {
+//			// Get UserId from SessionId
+//			LOG.info("Trying to retrieve user from session id");
+//			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
+//			// Retrieving user details
+//			LOG.info("Trying to retrieve featured top books for user");
+//			Book[] books = QueryDatabase.getFeaturedBooks(profile);
+//			String json = gson.toJson(books);
+//			LOG.info("Response json: " + json);
+//			return json;
+//		} catch (Exception ex) {
+//			LOG.error("Failed to retrieve top featured books", ex);
+//			return gson.toJson("Failed");
+//		}
+//	}
+//
+//	@Path("getAllBooks")
+//	@POST
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//	public String getAllBooks(@FormParam("sessionId") String sessionId,
+//							  @FormParam("dimension") String dimension,
+//							  @FormParam("pageNo") String page) {
+//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//		try {
+//			// Get UserId from SessionId
+//			LOG.info("Trying to retrieve user from session id");
+//			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
+//			// Retrieving user details
+//			LOG.info("Trying to retrieve all books for user");
+//			Book[] books = QueryDatabase.getAllBooks();
+//			String json = gson.toJson(books);
+//			//String json = gson.toJson("NOT HERE");
+//			LOG.info("Response json: " + json);
+//			return json;
+//		} catch (Exception ex) {
+//			LOG.error("Failed to retrieve all books", ex);
+//			return gson.toJson("Failed"+ex.toString());
+//		}
+//	}
+//
+//	@Path("getLibrary")
+//	@POST
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//	public String getLibrary(@FormParam("sessionId") String sessionId,
+//							  @FormParam("libraryId") String libraryId) {
+//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//		try {
+//			// Get UserId from SessionId
+//			LOG.info("Trying to retrieve user from session id");
+//			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
+//			// Retrieving user details
+//			LOG.info("Trying to retrieve library");
+//			Library library = QueryDatabase.getLibraryById(profile, libraryId);
+//			String json = gson.toJson(library);
+//			LOG.info("Response json: " + json);
+//			return json;
+//		} catch (Exception ex) {
+//			LOG.error("Failed to retrieve library", ex);
+//			return gson.toJson("Failed");
+//		}
+//	}
+//
+//	@Path("getAllLibraries")
+//	@POST
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//	public String getAllLibraries(@FormParam("sessionId") String sessionId,
+//								  @FormParam("pageNo") String page) {
+//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//		try {
+//			// Get UserId from SessionId
+//			LOG.info("Trying to retrieve user from session id");
+//			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
+//			// Retrieving user details
+//			Library [] libraries = QueryDatabase.getAllLibraries(profile, page);
+//			String json = gson.toJson(libraries);
+//			LOG.info("Response json: " + json);
+//			return json;
+//		} catch (Exception ex) {
+//			LOG.error("Failed to retrieve library", ex);
+//			return gson.toJson("Failed");
+//		}
+//	}
+//
+//	@Path("getLibrariesByPrefix")
+//	@POST
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//	public String getLibrariesByPrefix(@FormParam("sessionId") String sessionId,
+//									   @FormParam("prefix") String prefix) {
+//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//		try {
+//			// Get UserId from SessionId
+//			LOG.info("Trying to retrieve user from session id");
+//			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
+//			// Retrieving user details
+//			Library [] libraries = QueryDatabase.getLibraryByPrefix(profile, prefix);
+//			String json = gson.toJson(libraries);
+//			LOG.info("Response json: " + json);
+//			return json;
+//		} catch (Exception ex) {
+//			LOG.error("Failed to retrieve library", ex);
+//			return gson.toJson("Failed");
+//		}
+//	}
+//
+//	@Path("subscribe")
+//	@POST
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//	public String subscribe(@FormParam("sessionId") String sessionId,
+//							@FormParam("libraryId") String libraryId
+//	) {
+//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//		try {
+//			// Get UserId from SessionId
+//			LOG.info("Trying to retrieve user id from session id");
+//			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
+//			LOG.info("Trying to subscribe");
+//			boolean status = QueryDatabase.subscribeLibrary(profile, libraryId);
+//			return gson.toJson(status ? "Success" : "Failure");
+//		} catch (Exception ex) {
+//			LOG.error("Failed to subscribe", ex);
+//			return gson.toJson("Failed");
+//		}
+//	}
+//
+//	@Path("unsubscribe")
+//	@POST
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//	public String unsubscribe(@FormParam("sessionId") String sessionId,
+//							@FormParam("libraryId") String libraryId
+//	) {
+//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//		try {
+//			// Get UserId from SessionId
+//			LOG.info("Trying to retrieve user id from session id");
+//			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
+//			LOG.info("Trying to un-subscribe");
+//			boolean status = QueryDatabase.unSubscribeLibrary(profile, libraryId);
+//			return gson.toJson(status ? "Success" : "Failure");
+//		} catch (Exception ex) {
+//			LOG.error("Failed to unsubscribe", ex);
+//			return gson.toJson("Failed");
+//		}
+//	}
 }
--- gapelia-core/src/main/java/com/gapelia/core/api/Notification.java
+++ gapelia-core/src/main/java/com/gapelia/core/api/Notification.java
@@ -12,24 +12,24 @@ import javax.ws.rs.core.MediaType;
 @Path("/notification")
 public class Notification {
 	public static Logger LOG = Logger.getLogger(Notification.class);
-
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public String getNotification(@FormParam("sessionId") String sessionId) {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		try {
-			// Get UserId from SessionId
-			LOG.info("Trying to retrieve user from session id");
-			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
-			// Retrieving user details
-			LOG.info("Trying to retrieve notifications for user");
-			LOG.info("Response json: ");
-            return gson.toJson("Failed");
-		} catch (Exception ex) {
-			LOG.error("Failed to retrieve notifications", ex);
-			return gson.toJson("Failed");
-		}
-	}
+//
+//	@POST
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//	public String getNotification(@FormParam("sessionId") String sessionId) {
+//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//		try {
+//			// Get UserId from SessionId
+//			LOG.info("Trying to retrieve user from session id");
+//			org.brickred.socialauth.Profile profile = AuthHelper.getUserProfileFromSessionId(sessionId);
+//			// Retrieving user details
+//			LOG.info("Trying to retrieve notifications for user");
+//			LOG.info("Response json: ");
+//            return gson.toJson("Failed");
+//		} catch (Exception ex) {
+//			LOG.error("Failed to retrieve notifications", ex);
+//			return gson.toJson("Failed");
+//		}
+//	}
 
 }
