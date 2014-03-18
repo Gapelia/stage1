2daf9c72778143c389b4ae18d4d5e53a074a8c8b
Daniel Campos <dfcspoor@hotmail.com>
2014-03-18 11:52:15 -0400

    frankies stuff

--- gapelia-core/src/main/java/com/gapelia/core/auth/SocialLogin.java
+++ gapelia-core/src/main/java/com/gapelia/core/auth/SocialLogin.java
@@ -14,41 +14,46 @@ import static com.gapelia.core.auth.AuthHelper.APP_FACEBOOK;
 import static com.gapelia.core.auth.AuthHelper.APP_GOOGLE;
 
 public class SocialLogin extends HttpServlet {
-	public static Logger LOG = Logger.getLogger(SocialLogin.class);
-	private static final long serialVersionUID = 1L;
+    public static Logger LOG = Logger.getLogger(SocialLogin.class);
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			SocialAuthConfig config = SocialAuthConfig.getDefault();
-			config.load();
-			SocialAuthManager manager = new SocialAuthManager();
-			manager.setSocialAuthConfig(config);
-			String hostName = null;
-			String mode = null;
-			try {
-				mode = System.getProperty("gapeliaMode");
-			} catch (Exception ex) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            SocialAuthConfig config = SocialAuthConfig.getDefault();
+            SocialAuthManager manager = new SocialAuthManager();
 
+            String mode = null;
+            String hostName;
+            try {
+                mode = System.getProperty("gapeliaMode");
+            } catch (Exception ex) {
+                // Ignore mode is null
             }
-			if (null != mode && "local".equals(mode)) {
-				hostName = "http://localhost:8080";
-			} else {
-				hostName = "http://gapeliaalpha-1815355907.us-east-1.elb.amazonaws.com/";
-			}
-			String successUrl = hostName + "/success;jsessionid=" + request.getSession().getId();
-			String type = request.getParameter("type");
-			String url = null;
-			if (APP_FACEBOOK.equals(type)) {
-				url = manager.getAuthenticationUrl(APP_FACEBOOK, successUrl, Permission.AUTHENTICATE_ONLY);
-			} else {
-				url = manager.getAuthenticationUrl(APP_GOOGLE, successUrl, Permission.AUTHENTICATE_ONLY);
-			}
-			HttpSession session = request.getSession();
-			session.setAttribute("authManager", manager);
-			response.sendRedirect(response.encodeRedirectURL(url));
-		} catch (Exception e) {
-			throw new ServletException(e);
-		}
-	}
+            if (null != mode && "local".equals(mode)) {
+                hostName = "http://localhost:8080";
+                config.load("oauth_consumer_local.properties");
+            } else {
+                config.load();
+                hostName = "http://gapeliaalpha-1815355907.us-east-1.elb.amazonaws.com/";
+            }
+            manager.setSocialAuthConfig(config);
+
+            String successUrl = hostName + "/success;jsessionid=" + request.getSession().getId();
+            String type = request.getParameter("type");
+
+            String url = null;
+            if (APP_FACEBOOK.equals(type)) {
+                url = manager.getAuthenticationUrl(APP_FACEBOOK, successUrl, Permission.AUTHENTICATE_ONLY);
+            } else {
+                url = manager.getAuthenticationUrl(APP_GOOGLE, successUrl, Permission.AUTHENTICATE_ONLY);
+            }
+
+            HttpSession session = request.getSession();
+            session.setAttribute("authManager", manager);
+            response.sendRedirect(response.encodeRedirectURL(url));
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
 
 }
