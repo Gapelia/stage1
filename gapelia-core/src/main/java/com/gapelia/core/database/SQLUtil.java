429474d3ec151955ffe0300438a53244e3453bb2
liuzzi <liuzzi.f@ccs.neu.edu>
2014-03-18 15:15:45 -0400

    fixed date issues with login

--- gapelia-core/src/main/java/com/gapelia/core/database/SQLUtil.java
+++ gapelia-core/src/main/java/com/gapelia/core/database/SQLUtil.java
@@ -1,11 +1,17 @@
 package com.gapelia.core.database;
 import org.brickred.socialauth.util.BirthDate;
+import org.joda.time.LocalDate;
 
 import java.sql.Date;
+import java.util.Calendar;
 
 public class SQLUtil {
     public static Date convertBirthDate(BirthDate bd){
-        Date date = new Date(bd.getYear(),bd.getMonth(),bd.getDay());
-        return date;
+
+		Calendar cal  = Calendar.getInstance();
+
+		cal.set(bd.getYear(),bd.getMonth(),bd.getDay());
+		LocalDate date = new LocalDate(bd.getYear(),bd.getMonth(),bd.getDay());
+		return new Date(date.toDate().getTime());
     }
 }
