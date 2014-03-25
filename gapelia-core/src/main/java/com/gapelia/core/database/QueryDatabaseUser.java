f4422eedf09ef2264fc5b679c65d6114408121c9
liuzzi <liuzzi.f@ccs.neu.edu>
2014-03-25 02:04:57 -0400

    revert

--- gapelia-core/src/main/java/com/gapelia/core/database/QueryDatabaseUser.java
+++ gapelia-core/src/main/java/com/gapelia/core/database/QueryDatabaseUser.java
@@ -88,7 +88,7 @@ public class QueryDatabaseUser {
 			}
 
             insert.setString(6, p.getLocation());
-            insert.setString(7, p.getProfileImageURL() + "?width=1000&height=1000");
+            insert.setString(7, p.getProfileImageURL() );
             insert.setString(8, p.getFirstName());
             insert.setString(9, p.getValidatedId());
             insert.setString(10, p.getProviderId());
