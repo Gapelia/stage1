8d2dc515ea681309beebca391824e746e25cc567
Tommy lame huevos <tommy@gapelia.com>
2014-04-25 13:14:21 -0400

    email share

--- gapelia-core/src/main/webapp/static/scripts/ajax.js
+++ gapelia-core/src/main/webapp/static/scripts/ajax.js
@@ -528,7 +528,7 @@ function getLibrary() {
             currentWebsite = document.URL;
             facebookShare = 'http://www.facebook.com/sharer/sharer.php?u=' + currentWebsite;
             twitterShare = 'http://twitter.com/share?url=' + currentWebsite + 'is an exceptionally gratifying read on Gapelia';
-            emailShare = 'mailto:?subject=Recommended%20Read&amp;body=Check this out:  ' + currentWebsite;
+            emailShare = 'mailto:?subject=Recommended%20Library&amp;body=This is an exceptional library you should contribute to:  ' + currentWebsite;
 	    toInsert = "<section id=\"library-splash\" class=\"imgLiquid_bgSize imgLiquid_ready\" style=\"background-image: url(" + library.coverPhoto + "); background-size: cover; background-position: 50% 50%; background-repeat: no-repeat no-repeat;\">";
 	    toInsert += "<div id=\"library-info\">";
             if (libraryId in subscribed == true) {
