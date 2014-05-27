b9a8ab3609257f4720c42e2eec92d263f4daf299
liuzzi <liuzzi.f@ccs.neu.edu>
2014-05-26 21:15:41 -0400

    first try feedback form

--- gapelia-core/src/main/webapp/static/scripts/feedback.js
+++ gapelia-core/src/main/webapp/static/scripts/feedback.js
@@ -122,41 +122,25 @@
 				jQuery('.contactable-holder').hide();
 				jQuery('#contactable-loading').show();
 
-				// Trigger form submission if form is valid
-				jQuery.ajax({
-					type: 'POST',
-					url: options.url,
-					data: {
-						subject:options.subject,
-						name:jQuery('#contactable-name').val(),
-						email:jQuery('#contactable-email').val(),
-						issue:jQuery('#contactable-dropdown').val(),
-						message:jQuery('#contactable-message').val()
-					},
-					success: function(data) {
-						// Hide loading animation
-						jQuery('#contactable-loading').css({display:'none'});
 
-						// Check for a valid server side response
-						if( data.response === 'success') {
-							jQuery('#contactable-callback').show().append(options.recievedMsg);
-							if(options.hideOnSubmit === true) {
-								//hide the tab after successful submition if requested
-								jQuery('#contactable-inner').click();
-							}
-						} else {
-							jQuery('#contactable-callback').show().append(options.notRecievedMsg);
-							setTimeout(function(){
-								jQuery('.contactable-holder').show();
-								jQuery('#contactable-callback').hide().html('');
-							},2000);
-						}
-					},
-					error:function(e){
-						jQuery('#contactable-loading').css({display:'none'});
-						jQuery('#contactable-callback').show().append(options.notRecievedMsg);
-					}
-				});
+				$.ajax({
+                        url: "/api/email/sendFeedbackEmail",
+                        contentType: "application/x-www-form-urlencoded;charset=utf-8",
+                        type: "POST",
+                async: false,
+                        data: {
+                            sessionId: sessionId,
+                            name: $('#contactable-name').val(),
+                            email: $('#contactable-email').val(),
+                            message: $('#contactable-message').val()
+                                                    },
+                        error: function (q, status, err) {
+                            if (status == "timeout") {
+                                alert("Request timed out");
+                            }
+                        }
+                    });
+
 			}
 		});
 	};
