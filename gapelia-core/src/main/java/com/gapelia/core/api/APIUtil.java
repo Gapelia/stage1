a2b61a0e20201db8c3c4d7b37084065ae2999a8c
liuzzi <liuzzi.f@ccs.neu.edu>
2014-05-28 14:48:42 -0400

    searchapi bugfix

--- gapelia-core/src/main/java/com/gapelia/core/api/APIUtil.java
+++ gapelia-core/src/main/java/com/gapelia/core/api/APIUtil.java
@@ -11,97 +11,100 @@ import org.apache.log4j.Logger;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.sql.Connection;
 import java.util.ArrayList;
 
 
 @Path("/utils/")
 public class APIUtil {
-    private static Logger LOG = Logger.getLogger(APIUtil.class);
+	private static Logger LOG = Logger.getLogger(APIUtil.class);
 
-    public static String INVALID_SESSION_ERROR_MSG = "SessionID is not valid/Associated with a user";
+	public static String INVALID_SESSION_ERROR_MSG = "SessionID is not valid/Associated with a user";
 
-    public static boolean isValidSession(String sessionId) {
-        if (SessionManager.getUserFromSessionId(sessionId) != null) {
-            return true;
-        }
-        return false;
-    }
+	public static boolean isValidSession(String sessionId) {
+		if (SessionManager.getUserFromSessionId(sessionId) != null) {
+			return true;
+		}
+		return false;
+	}
 
-    public static boolean checkIfOwnBook() {
-        return false;
-    }
+	public static boolean checkIfOwnBook() {
+		return false;
+	}
 
-    public static boolean checkIfOwnUser() {
-        return false;
-    }
+	public static boolean checkIfOwnUser() {
+		return false;
+	}
 
-    public static boolean checkIfOwnLibrary() {
-        return false;
-    }
+	public static boolean checkIfOwnLibrary() {
+		return false;
+	}
 
-    public static boolean checkIfOwnPage() {
-        return false;
-    }
-    @Path("getUserFromLibraryId")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public String getUserFromLibraryId(@FormParam("libraryId") int libraryId) {
-        Gson gson = new GsonBuilder().create();
-        return gson.toJson(QueryUtils.getUserFromLibraryId(libraryId));
-    }
+	public static boolean checkIfOwnPage() {
+		return false;
+	}
+	@Path("getUserFromLibraryId")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String getUserFromLibraryId(@FormParam("libraryId") int libraryId) {
+		Gson gson = new GsonBuilder().create();
+		return gson.toJson(QueryUtils.getUserFromLibraryId(libraryId));
+	}
 
-    @Path("isUserNameAvailable")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public String getUserFromLibraryId(@FormParam("userName") String userName) {
-        Gson gson = new GsonBuilder().create();
-        return gson.toJson(QueryUtils.checkUserByName(userName));
-    }
+	@Path("isUserNameAvailable")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String getUserFromLibraryId(@FormParam("userName") String userName) {
+		Gson gson = new GsonBuilder().create();
+		return gson.toJson(QueryUtils.checkUserByName(userName));
+	}
 
-    @Path("getUserFromBookId")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public String getUserFromBookId(@FormParam("bookId") int bookId) {
-        Gson gson = new GsonBuilder().create();
-        return gson.toJson(QueryUtils.getUserFromBookId(bookId));
-    }
+	@Path("getUserFromBookId")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String getUserFromBookId(@FormParam("bookId") int bookId) {
+		Gson gson = new GsonBuilder().create();
+		return gson.toJson(QueryUtils.getUserFromBookId(bookId));
+	}
 
-    @Path("getBookFromBookId")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public String getBookFromBookId(@FormParam("bookId") int bookId) {
-        Gson gson = new GsonBuilder().create();
-        return gson.toJson(QueryUtils.getBookFromBookId(bookId));
-    }
+	@Path("getBookFromBookId")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String getBookFromBookId(@FormParam("bookId") int bookId) {
+		Gson gson = new GsonBuilder().create();
+		return gson.toJson(QueryUtils.getBookFromBookId(bookId));
+	}
 
-    @Path("getLibraryFromBookId")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public String getLibraryFromBookId(@FormParam("bookId") int bookId) {
-        Gson gson = new GsonBuilder().create();
-        return gson.toJson(QueryUtils.getLibraryFromBookId(bookId));
-    }
+	@Path("getLibraryFromBookId")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String getLibraryFromBookId(@FormParam("bookId") int bookId) {
+		Gson gson = new GsonBuilder().create();
+		return gson.toJson(QueryUtils.getLibraryFromBookId(bookId));
+	}
 
 	@GET
 	@Path("search/{query}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String searchQuery(@PathParam("query") String query) {
+	public Response searchQuery(@PathParam("query") String query) {
 		Gson gson = new GsonBuilder().create();
 		LOG.info("SEARCHING FOR:"+query);
-		Book b = new Book();
-		b.setTitle(query);
-		b.setBookId(777);
-
 
 		Search s = new Search();
-		s.addBook(b);
-		return gson.toJson(s);
+		for(int i = 0; i < query.length();i++){
+			Book b = new Book();
+			b.setTitle(query);
+			b.setBookId(i);
+			s.addBook(b);
+		}
+
+		return Response.ok(gson.toJson(s), MediaType.APPLICATION_JSON).header("Access-Control-Allow-Origin", "*").build();
 
 	}
 }
